47ce7d6764bf0aef8028d75e9144b6a1b37575c6
Brandon <duongb9@mcmaster.ca>
2021-06-12 14:43:46 -0400

    Profile now shows a user's reviews

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {Container} from "react-bootstrap";
+import {Container, ListGroupItem} from "react-bootstrap";
 import {useParams} from "react-router";
+import {Card} from "@material-ui/core";
+import {DataGrid} from "@material-ui/data-grid";
 
 function Profile(props) {
     const { user } = useParams()
 
     const [join_date, setJoinDate] = useState("")
     const [profileFound, setProfileFound] = useState(false)
+
+    const [lists, setLists] = useState([])
+    const [listsFound, setListsFound] = useState(false)
 
 
     useEffect(() => {
@@ -37,15 +42,54 @@ function Profile(props) {
                 console.error(err);
                 // Bring up 404 page not found
             })
+
+        fetch('/api/getList/' + user, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+
+                } else {
+                    const error = new Error(res.error);
+                    throw error;
+                }
+            })
+            .then(data => {
+                console.log(data)
+                setLists([...data.data])
+                setListsFound(true)
+            })
+            .catch(err => {
+                console.error(err);
+                // Bring up 404 page not found
+            })
     }, [user])
+
+    const columns = [
+        // {field: 'title', headerName: 'test', flex: 1},
+        { field: 'gameId', headerName: 'Title', width: 625, flex: 1},
+        { field: 'rating', headerName: 'Rating', width: 450, flex: 1 },
+        { field: 'thoughts', headerName: 'Thoughts', width: 450, flex: 1 },
+        { field: 'hours', headerName: 'Hours', width: 450, flex: 1 },
+    ]
 
     return (
         <Container fluid={"sm"}>
-            {profileFound ?
+            {profileFound && listsFound ?
                 <span>
                     User: {user}
                         <br/>
                     Joined: {join_date}
+                    <br/>
+
+                    <div style={{ height: 765, width: '100%' }}>
+                        <DataGrid columns={columns} rows={lists} columnBuffer={50} rowHeight={50} pageSize={25}
+                                  getRowId={(row) => row._id}/>
+                    </div>
                 </span>
 
                 :
--- server/controllers/videogamelist-ctrl.js
+++ server/controllers/videogamelist-ctrl.js
@@ -39,11 +39,6 @@ getList = async (req, res) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
         }
-        if (!reviews.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Reviews not found` })
-        }
         return res.status(200).json({ success: true, data: reviews })
     }).catch(err => console.log(err))
 }
